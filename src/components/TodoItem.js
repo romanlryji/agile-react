c553c0b4bec2c1b956a7326ed2465ccc17807354
Рыжий Роман Леонидович <r.ryzhiy@ase-ec.ru>
2021-08-26 14:28:18 +0300

    TodoItem as InputGroup

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,23 @@
-// import InputGroup from 'react-bootstrap/InputGroup'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 
 const TodoItem = props => {
   const { completed, id, title } = props.todo
 
   return (
-    <div>{title} <Button onClick={() => { props.deleteTodoFunc(id) }}>Delete</Button></div>
+    <InputGroup>
+      <FormControl
+        value={title}
+      />
+      <Button
+        variant="outline-secondary"
+        id="button-addon2"
+        onClick={() => { props.deleteTodoFunc(id) }}
+      >
+        Delete
+      </Button>
+    </InputGroup>
   )
 }
 
